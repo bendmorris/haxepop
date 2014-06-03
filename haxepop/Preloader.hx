8214dd78e1ab7b748d85f0aef7b6e4bfd697dc89
bendmorris <ben@bendmorris.com>
2014-06-02 21:19:12 -0700

    Updating preloader.

--- haxepop/Preloader.hx
+++ haxepop/Preloader.hx
@@ -1,14 +1,6 @@
 package haxepop;
 
-#if doc
-
-#elseif js
-
-// html5 doesn't support drawTiles
-class Preloader extends NMEPreloader { }
-
-#else
-
+import haxe.Timer;
 import flash.display.Bitmap;
 import flash.display.BitmapData;
 import flash.display.Graphics;
@@ -16,75 +8,77 @@ import flash.display.Sprite;
 import flash.events.Event;
 import flash.geom.Point;
 import flash.geom.Rectangle;
+import haxepop.HXP;
+import haxepop.graphics.Image;
 
-import openfl.display.Tilesheet;
 
-@:bitmap("assets/graphics/preloader/haxepunk.png")
-class HaxePunkLogo extends BitmapData {}
+@:bitmap("assets/graphics/preloader/haxepop.png")
+class Logo extends BitmapData {}
 
 class Preloader extends NMEPreloader
 {
+	public var img:Bitmap;
+	public var timer:Timer;
+	public var fadeout_time:Float=0;
+	public static inline var FADEOUT_TIME:Float=1.25;
 
 	public function new()
 	{
-		tileData = [
-			  0,   0, 0, 1,  0, // powered by
-			 80, 190, 3, 1, 20, // large cog
-			190, 185, 4, 1, 40, // small cog
-			187, 260, 2, 1,  0, // factory
-			  0, 260, 1, 1,  0, // haxepunk
-		];
-
-		tiles = new Tilesheet(new HaxePunkLogo(0, 0));
-		tiles.addTileRect(new Rectangle(0, 0, 274, 58)); // powered by
-		tiles.addTileRect(new Rectangle(0, 65, 274, 80)); // haxepunk
-		tiles.addTileRect(new Rectangle(0, 165, 114, 190), new Point(57, 190)); // factory
-		tiles.addTileRect(new Rectangle(115, 164, 134, 136), new Point(67, 68)); // large cog
-		tiles.addTileRect(new Rectangle(123, 305, 56, 56), new Point(28, 28)); // small cog
-
-		scaleIncrement = 0.002;
-
-		var width = 260;
-		var height = 340;
-
-		// update bar position
-		var color = 0xCB6325;
-		var padding = 5;
+		img = new Bitmap(new Logo(0,0));
+		var w = 256;
+		var h = 64;
 
 		super();
 
-		outline.x = (getWidth() - width) / 2;
-		outline.y = (getHeight() - height) / 2;
-		outline.graphics.clear();
-
-		progress.y = outline.y + 60;
-		progress.x = outline.x + width / 2;
-		progress.graphics.clear();
-		progress.graphics.lineStyle(1, color);
-		progress.graphics.moveTo(-width / 2 + padding, 0);
-		progress.graphics.lineTo(width / 2 - padding, 0);
-
-		addEventListener(Event.ENTER_FRAME, onEnterFrame);
-		onEnterFrame(null); // initial render
+		outline.y += 40;
+		progress.y += 40;
+		
+		img.x = (getWidth() - w) / 2;
+		img.y = (getHeight() - h) / 2 - 20;
+		
+		addChild(img);
 	}
 
-	public function onEnterFrame(e:Event)
+	override public function onInit()
 	{
-		tileData[9] += 1; // large cog
-		tileData[14] += 1; // small cog
-		tileData[18] += scaleIncrement; // factory scale
-		if (tileData[18] > 1.02 || tileData[18] < 1)
-			scaleIncrement = -scaleIncrement;
-
-		outline.graphics.clear();
-		tiles.drawTiles(outline.graphics, tileData, true, Tilesheet.TILE_ROTATION | Tilesheet.TILE_SCALE);
+		stage.color = 0xFFFFFF;
 	}
 
-	private var scaleIncrement:Float;
+	override public function onUpdate(bytesLoaded:Int, bytesTotal:Int)
+	{
+		super.onUpdate(bytesLoaded, bytesTotal);
 
-	private var tiles:Tilesheet;
-	private var tileData:Array<Float>;
+		var percentLoaded = bytesLoaded / bytesTotal;
+		img.alpha = HXP.clamp(percentLoaded, 0.2, 1);
+	}
+
+	override public function onLoaded()
+	{
+		img.alpha = 1;
+		timer = new Timer(Std.int(1000/30));
+		timer.run = fadeout;
+	}
+
+	function fadeout()
+	{
+		fadeout_time += 1/30/FADEOUT_TIME*2.5;
+		if (fadeout_time >= 1) {
+			outline.alpha = progress.alpha = img.alpha = progress.alpha = Math.max(0, img.alpha - 1/30/FADEOUT_TIME*2.5);
+			if (img.alpha <= 0 && fadeout_time > 2.5) {
+				timer.stop();
+				done();
+			}
+		}
+	}
+
+	override public function getBackgroundColor():Int
+	{
+		return 0xFFFFFF;
+	}
+
+	function done()
+	{
+		dispatchEvent (new Event (Event.COMPLETE));
+	}
 
 }
-
-#end
